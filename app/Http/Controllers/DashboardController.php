8bd621ca71071b05b3db8c0c86902e958a9e51fc
Ferreira Jr <ferreirajr026@gmail.com>
2025-05-13 12:57:29 -0300

    feat: Cria o controlador DashboardController para gerenciar dados do usuário e transações

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+
+class DashboardController extends Controller
+{
+    public function dashboard()
+    {
+        $user = Auth()->user()->load('wallet');
+
+        $walletId = $user->wallet->id;
+
+        $startOfMonth = now()->startOfMonth();
+        $endOfMonth = now()->endOfMonth();
+
+        $depositCount = $user->wallet->transactions()
+            ->where('type', 'deposit')
+            ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
+            ->count();
+        $transferCount = Transaction::where('type', 'transfer')
+            ->where('wallet_id_from', $walletId)
+            ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
+            ->count();
+
+        $lastDeposits = $user->wallet->transactions()
+            ->where('type', 'deposit')
+            ->orderByDesc('created_at')
+            ->take(10)
+            ->get()
+            ->map(function ($deposit) {
+                return [
+                    'id' => $deposit->id,
+                    'type' => $deposit->type,
+                    'amount' => $deposit->amount,
+                    'formatted_amount' => 'R$ ' . number_format($deposit->amount, 2, ',', '.'),
+                    'created_at' => $deposit->created_at,
+                    'formatted_created_at' => $deposit->created_at->format('d/m/Y H:i:s'),
+                ];
+            })
+            ->toArray();
+
+        $lastTransfers = Transaction::where('type', 'transfer')
+            ->where('wallet_id_from', $walletId)
+            ->orderByDesc('created_at')
+            ->take(10)
+            ->get()
+            ->map(function ($transfer) {
+                return [
+                    'id' => $transfer->id,
+                    'type' => $transfer->type,
+                    'amount' => $transfer->amount,
+                    'formatted_amount' => 'R$ ' . number_format($transfer->amount, 2, ',', '.'),
+                    'created_at' => $transfer->created_at,
+                    'formatted_created_at' => $transfer->created_at->format('d/m/Y H:i:s'),
+                ];
+            })
+            ->toArray();
+
+        return inertia('Dashboard', [
+            'user' => $user,
+            'balance' => $user->wallet->getFormattedBalanceAttribute,
+            'depositCount' => $depositCount,
+            'transferCount' => $transferCount,
+            'lastDeposits' => $lastDeposits,
+            'lastTransfers' => $lastTransfers,
+        ]);
+    }
+}
