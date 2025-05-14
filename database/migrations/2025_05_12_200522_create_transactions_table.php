b0a1590b001515624b1efeb3ac9ff548a67aed5c
Ferreira Jr <ferreirajr026@gmail.com>
2025-05-13 23:15:47 -0300

    feat: Adiciona tipo de transação 'reversal' na tabela de transações

--- database/migrations/2025_05_12_200522_create_transactions_table.php
+++ database/migrations/2025_05_12_200522_create_transactions_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignUuid('wallet_id')->constrained()->onDelete('cascade');
             $table->foreignUuid('wallet_id_from')->nullable()->constrained('wallets')->onDelete('cascade')->nullable();
             $table->foreignUuid('wallet_id_to')->nullable()->constrained('wallets')->onDelete('cascade')->nullable();
-            $table->enum('type', ['deposit', 'withdraw', 'transfer']);
+            $table->enum('type', ['deposit', 'withdraw', 'transfer','reversal']);
             $table->decimal('amount', 10, 2);
             $table->boolean('reverted')->default(false);
             $table->timestamp('reverted_at')->nullable();
